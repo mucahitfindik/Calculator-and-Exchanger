471c73b6a4a0cc001f36a8877423e2f88fe025d9
mucahitfindik <mucahitfindik97@gmail.com>
2022-06-23 02:35:19 +0200

    added exchange end point in Nodejs

--- nodejs_app/routes/index.js
+++ nodejs_app/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 const calculationController  = require('../controllers/calculation');
+const exchangeController  = require('../controllers/exchange');
 var router = express.Router();
 
 router.get('/calculate', calculationController.calculation);
+router.get('/exchange', exchangeController.exchange);
 module.exports = router;
