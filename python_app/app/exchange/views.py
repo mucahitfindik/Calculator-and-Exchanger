f8768038c034fc9e4859e7509bbe27ddeb6e4519
mucahitfindik <mucahitfindik97@gmail.com>
2022-06-23 03:31:29 +0200

    the exchange endpoint's response is fixed

--- python_app/app/exchange/views.py
+++ python_app/app/exchange/views.py
@@ -6,7 +6,14 @@ blueprint = Blueprint('exchange', __name__)
 @blueprint.route('/exchange', methods=('GET',))
 def get_articles():
     data = request.get_json()
+    if "amount" not in data or data["amount"] == "":
+        return {'result': "No amount provided!"}
+    elif "toCurrency" not in data or data["toCurrency"] == "":
+        return {'result': "No toCurrency provided!"}
+    elif "fromCurrency" not in data or data["fromCurrency"] == "":
+        return {'result': "No fromCurrency provided!"}
+
     amount = data["amount"]
     toCurrency = data["toCurrency"]
     fromCurrency = data["fromCurrency"]
-    return "Hello World!"
+    return {'result': "Hello World!"}
