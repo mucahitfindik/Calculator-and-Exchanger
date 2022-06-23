80568a7bf0ab0db87ad85f70142b6ae1289335d2
mucahitfindik <mucahitfindik97@gmail.com>
2022-06-23 04:07:57 +0200

    added test cases for the exchange end point

---
+++ nodejs_app/tests/exchangeEndPoint.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest')
+const app = require('../app')
+const pythonServerConfig = require("../controllers/pythonServerConfig")
+const sendHttp = require("../controllers/httpServer")
+describe('Exchange EndPoint Check', () => {
+    it('php_app calculate end point check', async() => {
+        const body = {
+            "amount" : 120, 
+            "toCurrency" : "TRY", 
+            "fromCurrency": "USD"
+        }
+        const res = await request("0.0.0.0:5002").get("/exchange").send(body)
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.result).toEqual("Hello World!")
+    })
+    it('should create a request with a body which includes expression', async() => {
+        const body = {
+            "amount" : 120, 
+            "toCurrency" : "TRY", 
+            "fromCurrency": "USD"
+        }
+        const res = await request(app).get("/exchange").send(body)
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.result).toEqual("Hello World!")
+    })
+    it('should create a request without a body', async() => {
+        const res = await request(app).get("/exchange")
+        expect(res.statusCode).toEqual(404)
+        expect(res.body.error).toEqual("Request As Not Expected!")
+    })
+    
+  })
