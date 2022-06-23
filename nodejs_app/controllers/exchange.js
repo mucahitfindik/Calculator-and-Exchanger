cf940b794001691fa1c4f8c4b1864f860e787f15
mucahitfindik <mucahitfindik97@gmail.com>
2022-06-23 03:59:20 +0200

    fixed issues

--- nodejs_app/controllers/exchange.js
+++ nodejs_app/controllers/exchange.js
@@ -2,13 +2,22 @@ const pythonServerConfig = require('../controllers/pythonServerConfig')
 const httpServer = require('../controllers/httpServer')
 const exchange = async (req, res, next) => {
     
+    if (!req.body.amount || !req.body.toCurrency || !req.body.fromCurrency ){
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'application/json');
+        return res.json({"error":"Request As Not Expected!"});
+    }
+    const data = JSON.stringify({
+        amount : req.body.amount,
+        toCurrency : req.body.toCurrency,
+        fromCurrency : req.body.fromCurrency,
+
+    });
     const options = pythonServerConfig.pythonServerConfig(path = req.path, method = req.method);
     options.headers = {
         'Content-Type': 'application/json',
+        'Content-Length': data.length
         };
-    const data = JSON.stringify({
-        expression : req.body.expression
-    });
     
     return httpServer.sendHttpRequest(res, options, data);
    /* const statusCode = statusCodeMessage[0];
