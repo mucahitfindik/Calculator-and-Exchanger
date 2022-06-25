6ab0f871154127ebad5f41f41050d9679740d3d0
mucahitfindik <mucahitfindik97@gmail.com>
2022-06-26 00:11:12 +0200

    added calculate function and fixed calculation controller

--- php_app/app/Http/Controllers/CalculationController.php
+++ php_app/app/Http/Controllers/CalculationController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Field_calculate;
+
 
 class CalculationController extends Controller
 {
     public function calculate(Request $request){
+        $calc = new Field_calculate();
         if(!$request->get("expression")){
             return response()->json(['message' => 'Expression Not Found!'], 404);
         }
-        $response = response()->json(['result' => '9']);
+        $exp = $request->get("expression");
+        if (!preg_match("#^[0-9 \.\+\-\*\/\(\)]+$#", $exp)) {
+            return response()->json(['message' => 'Expression includes unavailable character(s)'], 404); 
+        }
+        if (substr_count($exp, '(') != substr_count($exp, ')')) {
+            return response()->json(['message' => 'Parenthesis error!'], 404); 
+        }
+        $res = $calc->calculate($exp);
+        $response = response()->json(['result' => $res]);
         return $response;
     }
 }
