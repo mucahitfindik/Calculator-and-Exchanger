235a96a0206f50927ecd7c6f8f7f2c813d18cc6e
mucahitfindik <mucahitfindik97@gmail.com>
2022-06-28 02:46:17 +0200

    added currency list end point to nodejs side

--- nodejs_app/routes/index.js
+++ nodejs_app/routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 const calculationController  = require('../controllers/calculation');
 const exchangeController  = require('../controllers/exchange');
+const currency_listController  = require('../controllers/currency-list');
 const historyController  = require('../controllers/history');
 var router = express.Router();
 
 router.get('/calculate', calculationController.calculation);
 router.get('/exchange', exchangeController.exchange);
+router.get('/currency-list', currency_listController.currency_list);
 router.get('/history/formula', historyController.history_formula);
 router.get('/history/exchange', historyController.history_exchange);
 module.exports = router;
