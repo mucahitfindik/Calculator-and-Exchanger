888ffcde95733698f173a6f8f89d3f24a9e6729c
mucahitfindik <mucahitfindik97@gmail.com>
2022-07-06 00:43:24 +0200

    fixed issues

--- nodejs_app/routes/index.js
+++ nodejs_app/routes/index.js
@@ -5,8 +5,8 @@ const currency_listController  = require('../controllers/currency-list');
 const historyController  = require('../controllers/history');
 var router = express.Router();
 
-router.post('/calculate', calculationController.calculation);
-router.post('/exchange', exchangeController.exchange);
+router.get('/calculate', calculationController.calculation);
+router.get('/exchange', exchangeController.exchange);
 router.get('/currency-list', currency_listController.currency_list);
 router.get('/history/formula', historyController.history_formula);
 router.get('/history/exchange', historyController.history_exchange);
